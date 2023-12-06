6710c52e5af9f3431050d4a78f42f0332728996c
Camden <jacobdale98@aol.com>
2023-12-06 14:44:08 +0000

    Update home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
@@ -17,15 +16,14 @@ const Home: NextPage = () => {
             Add your metadata, with the amount of NFTs and batch of addresses you wish to send
           </p>
           <p className="text-center text-lg"> When happy with the parameters, create the NFT </p>
-          <p className="text-center text-lg"> Upload a csv file of addresses </p>
+          <p className="text-center text-lg"> Upload a csv file of addresses (Max 400) </p>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
+                Create your NFT using the{" "}
                 <Link href="/create" passHref className="link">
                   Create NFT
                 </Link>{" "}
@@ -33,11 +31,10 @@ const Home: NextPage = () => {
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
+                Multisend to multiple addresses with the{" "}
                 <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
+                  Multisend
                 </Link>{" "}
                 tab.
               </p>
